004d64b8b242e7fb4cd6f6d2273d9475295ced74
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-19 16:23:15 +0300

    Update App.js

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,36 +1,18 @@
-// App.js
-
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Login from './Login';
+import Notifications from './Notifications';
 
 class App extends Component {
-  static propTypes = {
-    logOut: PropTypes.func,
-  };
-
-  static defaultProps = {
-    logOut: () => {},
-  };
-
-  handleKeyDown = (event) => {
-    if (event.ctrlKey && event.key === 'h') {
-      alert('Logging you out');
-      this.props.logOut();
-    }
-  };
-
-  componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeyDown);
-  }
-
   render() {
     return (
       <div className="App">
-        {/* App content goes here */}
+        <Notifications />
+        <Header />
+        <Login />
+        <Footer />
       </div>
     );
   }
