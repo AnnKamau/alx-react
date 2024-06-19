9fb8f72e8f4f248086b23d09af335d7292bbb2e8
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-19 09:37:22 +0300

    Create notificationActionCreators.js

---
+++ 0x07-react_redux_action_creator_normalizr/ task_6/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,22 @@
+// notificationActionCreators.js
+import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+import { bindActionCreators } from 'redux';
+
+// Action Creators
+export function markAsRead(index) {
+  return {
+    type: MARK_AS_READ,
+    index
+  };
+}
+
+export function setNotificationFilter(filter) {
+  return {
+    type: SET_TYPE_FILTER,
+    filter
+  };
+}
+
+// Bound Action Creators
+export const boundMarkAsRead = (dispatch) => bindActionCreators(markAsRead, dispatch);
+export const boundSetNotificationFilter = (dispatch) => bindActionCreators(setNotificationFilter, dispatch);
