48d2996f8ceed2da64b3b0281d477fbab41752a5
AnnKamau <137866952+AnnKamau@users.noreply.github.com>
2024-06-21 09:08:08 +0300

    Create courses.js

---
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/schema/courses.js
@@ -0,0 +1,6 @@
+// courses.js
+import { schema, normalize } from 'normalizr';
+
+const course = new schema.Entity('courses');
+
+export const coursesNormalizer = (data) => normalize(data, [course]);
